7128d603d6d9c093b957ea0d317f11c8a0a5aea7
Lucas Jellema <lucasjellema@gmail.com>
2024-06-15 05:52:53 +0200

    add button extension

---
+++ add-button-extension/content.js
@@ -0,0 +1,4 @@
+// find body and add a button as its first element
+const body = document.querySelector("body");
+body.innerHTML = '<button id="add-button">My Big Fat Button</button>'+body.innerHTML;
+
