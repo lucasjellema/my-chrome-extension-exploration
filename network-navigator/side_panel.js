288a265293c21cb0481ea9bf91e6756111621db8
Lucas Jellema <lucasjellema@gmail.com>
2025-02-02 07:23:25 +0100

    moved procesing of linked in profile message to separate file

--- network-navigator/side_panel.js
+++ network-navigator/side_panel.js
@@ -1,5 +1,5 @@
 import { createEdge, createNode, findNodeByProperty } from './utils.js';
-
+import {processLinkedInProfile} from './processLinkedInProfile.js';
 // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 //   if (message.type === 'linkInfo') {
 //     const contentDiv = document.getElementById('content');
@@ -13,17 +13,24 @@ import { createEdge, createNode, findNodeByProperty } from './utils.js';
 //   }
 // });
 
+let cy
+
+document.addEventListener("cyInitialized", (event) => {
+  cy = event.detail;
+  console.log("Cytoscape instance injected into the module!", cy);
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
 
   if (message.type === 'linkedInProfile') {
 
-    processLinkedInProfile(message);
+    processLinkedInProfile(cy, message);
 
   }
 
   if (message.type === 'linkInfo') {
     console.log("Received linkInfoForNetwork message:", message);
-    addLink({
+    addLink(cy, {
       targetUrl: message.href,
       targetLabel: message.linkText,
       sourceUrl: message.sourceUrl,
@@ -44,14 +51,9 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-let cy
 
-document.addEventListener("cyInitialized", (event) => {
-  cy = event.detail;
-  console.log("Cytoscape instance injected into the module!", cy);
-});
 
-const addLink = (link) => {
+const addLink = (cy, link) => {
   const { targetUrl, targetLabel, sourceUrl, sourceTitle } = link;
   let sourceNode = findNodeByProperty(cy, 'url', sourceUrl);
   if (!sourceNode) {
@@ -71,53 +73,4 @@ const addLink = (link) => {
   edge.data('label', targetLabel);
   edge.data('type', 'hyperlink');
 };
-
-const processLinkedInProfile = (message) => {
-  const contentDiv = document.getElementById('content');
-  contentDiv.textContent = `
-        Profile Type: ${JSON.stringify(message.profile.type)} \n
-        Profile: ${JSON.stringify(message.profile)}
-        LinkedIn URL: ${message.linkedInUrl}
-      `;
-
-  const profile = message.profile;
-  if (profile.type === 'person') {
-    let personNode = findNodeByProperty(cy, 'label', profile.name);
-    if (!personNode) {
-      personNode = createNode(cy, profile.name);
-      personNode.data('url', message.linkedInUrl);
-      personNode.data('type', profile.type);
-      personNode.data('subtype', `linkedIn${profile.type}`);      
-    }
-    if (profile.image) personNode.data('image', profile.image);
-    personNode.data('about', profile.about);
-    if (profile.location) personNode.data('location', profile.location);
-
-    if (profile.currentCompany) {
-      let companyNode = findNodeByProperty(cy, 'label', profile.currentCompany);
-      if (!companyNode) {
-        companyNode = createNode(cy, profile.currentCompany);
-        companyNode.data('image', profile.currentCompanyLogo);
-        companyNode.data('type', 'company');
-        companyNode.data('shape', 'square');
-      }
-      const edge = createEdge(cy, personNode, companyNode);
-      edge.data('label', 'works at');
-      edge.data('type', 'workAt');
-      edge.data('currentRole', profile.currentRole);
-    }
-    if (profile.latestEducation) {
-      let educationNode = findNodeByProperty(cy, 'label', profile.latestEducation);
-      if (!educationNode) {
-        educationNode = createNode(cy, profile.latestEducation);
-        educationNode.data('image', profile.latestEducationLogo);
-        educationNode.data('type', 'education');
-        educationNode.data('shape', 'diamond');
-      }
-      const edge = createEdge(cy, personNode, educationNode);
-      edge.data('label', 'educated at');
-      edge.data('type', 'educatedAt');
-    }
-  }
-}
 
